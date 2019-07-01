b4b05048922143cacbee6c031cbeab779d6a68b8
Warhol WarholAndWallSt Wall St <WarholAndWallSt@iMac-3.local>
2019-07-01 14:16:50 -0400

    started work on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import * as firebase from '../fb';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public user: any = {};
 
-  constructor() { }
+  constructor(public authService: AuthService, public router: Router) { }
 
   ngOnInit() {
+    this.authService.getUserInfo().then(user => {
+      this.user = user;
+    })
+  }
+
+  logout()
+  {
+    firebase.default.auth().signOut().then(()=> {
+      this.router.navigateByUrl('landing');
+    })
   }
 
 }
